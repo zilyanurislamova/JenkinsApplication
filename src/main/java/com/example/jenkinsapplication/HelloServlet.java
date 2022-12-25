a92694ab6f80c5387e261e972f76d05432415b0d
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-25 17:41:05 +0400

    Changed World to Jenkins

--- src/main/java/com/example/jenkinsapplication/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello Jenkins!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
