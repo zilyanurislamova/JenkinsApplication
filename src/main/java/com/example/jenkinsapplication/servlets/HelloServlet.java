6b659ff59a75ef2fafbfa89e65694a07832acf9d
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-28 00:01:10 +0400

    hello from feature

--- src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello, Java! from feature";
+        message = "Hello, Java! from feature again";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
