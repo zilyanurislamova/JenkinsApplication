aa0b8c1420e54216fc6f14929cff7a2160ff2ee2
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-27 13:29:14 +0400

    changed HelloServlet.java index.jsp

--- src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
@@ -4,12 +4,12 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-//@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello Jenkins!";
+        message = "Hello, Jenkins!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
