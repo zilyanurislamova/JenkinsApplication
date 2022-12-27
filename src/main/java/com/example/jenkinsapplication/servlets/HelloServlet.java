3ab0360ca219dfa468033bc369e1fafa5e68ffef
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-27 13:40:06 +0400

    changed HelloServlet.java

--- src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
@@ -4,7 +4,7 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+//@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
