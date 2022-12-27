fc719a14dc1b787716a54999289c9233fb353e3d
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-28 00:26:50 +0400

    last hello for today

--- src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello, Java! from feature again and again";
+        message = "Hello, Java! last for today";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
