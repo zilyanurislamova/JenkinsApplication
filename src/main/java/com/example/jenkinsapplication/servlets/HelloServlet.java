bc01ca740251d578b94cf7036d0c364d8adc32c5
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-30 17:07:45 +0400

    updated HelloServlet

--- src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
@@ -1,13 +1,16 @@
 package com.example.jenkinsapplication.servlets;
 
-import java.io.*;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello, Jenkins!";
+        message = "Hello, Java!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
