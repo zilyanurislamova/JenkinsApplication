cf3becb3313dde2369cb89ce54b56dded932497e
Zilya Nurislamova <zilya.nurislamova@mail.ru>
2022-12-30 18:47:38 +0400

    updated HelloServlet

--- src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
+++ src/main/java/com/example/jenkinsapplication/servlets/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello, Jenkins!";
+        message = "Hello, Java!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
